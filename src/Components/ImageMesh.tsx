7aaf14786cb3eea679fda07e0fecdfd861438065
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-12-12 18:20:19 +0100

    move custom material

--- src/Components/ImageMesh.tsx
+++ src/Components/ImageMesh.tsx
@@ -6,54 +6,9 @@ import glsl from "babel-plugin-glsl/macro";
 import { useInterval } from 'usehooks-ts';
 import useAudioContext from "./Hooks/useAudioContext";
 import { ThreeElements, useLoader, extend, useFrame } from '@react-three/fiber';
-import { shaderMaterial } from "@react-three/drei";
+import ColorShiftMaterial from "./ColorShiftMaterial";
 
-const ColorShiftMaterial = shaderMaterial(
-  { uTime: 0, uTexture: null, frequencies: [0, 0.25, 0.75, 0.33, 1.0], uAmplitude: 1.0, uFilter: 50.0 },
-  // vertex shader
-  /*glsl*/`
-    varying vec2 vUv;
-    uniform float uTime;
-    uniform sampler2D uTexture;
-    uniform float frequencies[256];
-    uniform float uAmplitude;
-    uniform float uFilter;
-
-    void main() {
-      vUv = uv;
-      vec3 texture = texture2D(uTexture, uv).rgb;
-
-      float gray = (texture.r * 0.3 + texture.g * 0.59 + texture.b * 0.11);
-
-      float numberOfFrequencies = 256.0;
-      int frequencyIndex = int(floor(numberOfFrequencies * gray ) );
-
-      float frequency = frequencies[frequencyIndex];
-
-      vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-
-      float normalisedFrequency = frequency > uFilter ? (frequency / 255.0) : 0.0;
-      modelPosition.z += normalisedFrequency * uAmplitude;
-
-      vec4 viewPosition = viewMatrix * modelPosition;
-      vec4 projectedPosition = projectionMatrix * viewPosition;
-
-      gl_Position = projectedPosition;
-    }
-  `,
-  // fragment shader
-  /*glsl*/`
-    uniform sampler2D uTexture;
-    varying vec2 vUv;
-
-    void main() {
-      vec3 texture = texture2D(uTexture, vUv).rgb;
-      gl_FragColor = vec4(texture, 1.0);
-    }
-  `
-)
-
-// declaratively
+// to notify to three-js (it will not work without)
 extend({ ColorShiftMaterial })
 
 interface ImageMeshProps {
