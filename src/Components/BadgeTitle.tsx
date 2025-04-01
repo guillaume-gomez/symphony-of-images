27c340e30469923d776c631a98d9b95063a1b767
guillaume-gomez <guillaume.gomez846@gmail.com>
2025-04-01 10:54:19 +0200

    create a component to display instructrion

---
+++ src/Components/BadgeTitle.tsx
@@ -0,0 +1,15 @@
+
+interface BadgeTitleProps {
+	text: string;
+	number: number;
+}
+
+function BadgeTitle({text, number}: BadgeTitleProps) {
+	return (
+			<h2 className="text-lg font-bold flex flex-row gap-1 italic items-center">
+				<div className="badge badge-primary badge-lg rounded">{number}</div>
+				{text}
+			</h2>
+	);
+}
+export default BadgeTitle;
