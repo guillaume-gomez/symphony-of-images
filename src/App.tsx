be556b86e1d07e6a77726c7192390a1cc5916e2b
guillaume-gomez <guillaume.gomez846@gmail.com>
2025-04-13 13:35:58 +0200

    add option

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,14 @@ function App() {
         <AppContextProvider>
           <div className="flex flex-col gap-5">
             <Card title="Sound settings">
+              <div className="flex flex-row">
+                <div role="alert" className="alert alert-info alert-outline w-full">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="h-6 w-6 shrink-0 stroke-current">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                  </svg>
+                  <span>{!imageBase64 ?  "Upload an Image" : "Then, add sound or enable microphone"}.</span>
+                </div>
+              </div>
               <div className="flex w-full flex-col lg:flex-row">
                 <div className="card rounded-box grid h-44 grow place-items-center lg:w-6/12" style={{background: "var(--color-base-400)"}}>
                   <BadgeTitle number={1} text={"Upload an image"} />
@@ -39,9 +47,6 @@ function App() {
                   <Mp3Player />
                   <AudioPermission />
                 </div>
-              </div>
-              <div className="flex flex-row justify-center items-center">
-                <span>{!imageBase64 ?  "Upload an Image" : "Add sound"}</span>
               </div>
             </Card>
             {
